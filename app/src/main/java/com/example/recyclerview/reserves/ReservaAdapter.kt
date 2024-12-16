95dd5fdecd269c1b78313198ce39af265ac32047
davidfs-itic <davidfs.docencia@gmail.com>
2024-12-16 16:30:31 +0100

    Tercer pas: Creem el layout "plantilla" per a cada element del RecyclerView, a l'arxiu reserva_item.xml
    Implementem les classes Adapter i Holder.
    OnCreateViewHolder: retorna un holder
    OnBindViewHolder: Renderitza l'objecte en el view.
    
    Holder: realitza la renderització. Representa cada item del RecyclerView.

--- app/src/main/java/com/example/recyclerview/reserves/ReservaAdapter.kt
+++ app/src/main/java/com/example/recyclerview/reserves/ReservaAdapter.kt
@@ -1,24 +1,40 @@
 package com.example.recyclerview.reserves
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerview.R
+import org.w3c.dom.Text
 
-class ReservaAdapter:RecyclerView.Adapter<ReservaAdatperHolder>() {
+class ReservaAdapter(var llistatreserves:List<Reserva>):RecyclerView.Adapter<ReservaAdatperHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReservaAdatperHolder {
-        TODO("Not yet implemented")
+        var iteminflater=LayoutInflater.from(parent.context)
+        var recycleritem=iteminflater.inflate(R.layout.reserva_item,parent,false)
+        return ReservaAdatperHolder(recycleritem)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+       return llistatreserves.size
     }
 
     override fun onBindViewHolder(holder: ReservaAdatperHolder, position: Int) {
-        TODO("Not yet implemented")
+        val reserva=llistatreserves.get(position)
+        holder.Renderitzar(reserva)
     }
 }
 
 class ReservaAdatperHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    var imatgeMaterial:ImageView= itemView.findViewById(R.id.item_imatge_material)
+    var descripcioReserva:TextView=itemView.findViewById(R.id.item_descripcio_reserva)
+    var iniciReserva:TextView=itemView.findViewById(R.id.item_inici_reserva)
+    var fiReserva:TextView=itemView.findViewById(R.id.item_fi_reserva)
 
-
+    public fun Renderitzar(reserva: Reserva){
+        descripcioReserva.text=reserva.descripcio
+        iniciReserva.text=reserva.datareserva?.toStringFormat("yyyy-MM-dd")
+        fiReserva.text=reserva.datafinal?.toStringFormat("yyyy-MM-dd")
+    }
 }
