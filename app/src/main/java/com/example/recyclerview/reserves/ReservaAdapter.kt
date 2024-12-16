86c81fc35bd94429f16919090598860954706f2e
davidfs-itic <davidfs.docencia@gmail.com>
2024-12-16 15:46:43 +0100

    Segon pas, Creem les classes Adapter i Holder. Arxiu ReservaAdapter.kt

---
+++ app/src/main/java/com/example/recyclerview/reserves/ReservaAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.recyclerview.reserves
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ReservaAdapter:RecyclerView.Adapter<ReservaAdatperHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReservaAdatperHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ReservaAdatperHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+class ReservaAdatperHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+
+}
